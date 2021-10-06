37d997ad0dba1ce3dec637d634f2f27c520e36b6
kmichaeli <katarzyna.michaeli@play.pl>
2021-10-06 15:26:37 +0200

    first commit

---
+++ src/main/java/io/github/michaek/controller/TaskController.java
@@ -0,0 +1,35 @@
+package io.github.michaek.controller;
+
+import io.github.michaek.model.Task;
+import io.github.michaek.model.TaskRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+public class TaskController {
+    private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
+    private final TaskRepository repository;
+
+    TaskController(final TaskRepository repository){
+        this.repository = repository;
+    }
+
+    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
+    ResponseEntity<List<Task>> readAllTasks(){
+        logger.warn("Exposing all the tasks!");
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping(value = "/tasks")
+    ResponseEntity<List<Task>> readAllTasks(Pageable page){
+        logger.info("Custom pageable");
+        return ResponseEntity.ok(repository.findAll(page).getContent());
+    }
+}
---
+++ src/main/java/io/github/michaek/model/Task.java
@@ -0,0 +1,48 @@
+package io.github.michaek.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank(message="Task's description must not be empty")
+    private String description;
+    private boolean done;
+
+    Task(){
+
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return this.done;
+    }
+
+    void setDone(boolean done) {
+        this.done = done;
+    }
+
+}
